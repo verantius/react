808879b9c1a9484af0a321eec5f6248a077b83a5
Adam <adam.midtt@gmail.com>
2021-09-28 21:01:40 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from "./components/Todo";
+import Modal from "./components/Modal";
+import Backdrop from "./components/Backdrop";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React' />
+      <Todo text='Learn more React' />
+      <Todo text='Learn React' />
+      <Modal/>
+      <Backdrop/>
+    </div> 
   );
 }
 
---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+function Modal(){
+    return (
+        <div className='Modal'>
+            <p>are u sure ?</p>
+            <button className='btn btn--alt'>cancel</button>
+            <button className='btn'>confirm</button>
+        </div>
+    );
+}
+
+export default Modal;
